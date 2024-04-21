6ad985188577a01d689b9a56895846fb0a11899f
Khaledelhoseny <khaledelhoseny76@gmail.com>
2024-04-22 01:22:23 +0200

    changes in collections

--- src/main/java/Collections/Collections.java
+++ src/main/java/Collections/Collections.java
@@ -1,2 +1,24 @@
-package Collections;public class Collections {
+package Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Collections {
+    public static void main(String[] args) {
+        mapDemo();
+    }
+
+    public static void mapDemo(){
+        Map <String , Integer> fruitCalories = new HashMap() ;
+        fruitCalories.put("apple" ,95) ;
+        fruitCalories.put("lemon" ,20) ;
+        fruitCalories.put("banana" ,105) ;
+        fruitCalories.put("orange" ,45) ;
+        fruitCalories.put("orange" ,17) ;
+
+        //Emhanced For Loop
+        for(var item:fruitCalories.entrySet()){
+            System.out.println(item.getValue());
+        }
+
+    }
 }
